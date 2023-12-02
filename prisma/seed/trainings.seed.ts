7ff5eb0ec4e6dc8379d565322604edd6d12fd0e0
Hewerton Adão <hewerton80@gmail.com>
2023-12-02 08:08:46 -0300

    update seed

--- prisma/seed/trainings.seed.ts
+++ prisma/seed/trainings.seed.ts
@@ -2,8 +2,6 @@ import prisma from "../../src/lib/prisma";
 import { faker } from "@faker-js/faker";
 
 export async function seedTraingigs() {
-  const teachers = await prisma.user.findMany({ where: { isTeacher: true } });
-  const students = await prisma.user.findMany({ where: { isTeacher: false } });
   const exercises = await prisma.exercise.findMany();
   const exercisesTrainigsA = [...exercises].splice(0, 7).map((exercise) => ({
     exerciseId: exercise.id,
@@ -26,46 +24,48 @@ export async function seedTraingigs() {
     order: 1,
   }));
 
-  for (const teacher of teachers) {
-    for (const student of students) {
-      const studentHasTraining = await prisma.trainingPlan.count({
-        where: { studentId: student.id, teacherId: teacher.id },
-      });
-      if (!studentHasTraining) {
-        await prisma.trainingPlan.create({
-          data: {
-            name: faker.helpers.arrayElement([
-              "Treino para hipertrofia",
-              "Treino para emagrecimento",
-              "Treino para força",
-            ]),
-            studentId: student.id,
-            teacherId: teacher.id,
-            teacherEmail: teacher.email,
-            teacherName: teacher.name,
-            trainings: {
-              create: [
-                {
-                  name: "Treino A",
-                  trainingExercises: { create: exercisesTrainigsA },
-                },
-                {
-                  name: "Treino B",
-                  trainingExercises: { create: exercisesTrainigsB },
-                },
-                {
-                  name: "Treino C",
-                  trainingExercises: { create: exercisesTrainigsC },
-                },
-                {
-                  name: "Treino D",
-                  trainingExercises: { create: exercisesTrainigsD },
-                },
-              ],
-            },
+  const teachers = await prisma.user.findMany({ where: { isTeacher: true } });
+  const students = await prisma.user.findMany({ where: { isTeacher: false } });
+
+  for (const student of students) {
+    const studentHasTraining = await prisma.trainingPlan.count({
+      where: { studentId: student.id },
+    });
+    if (!studentHasTraining) {
+      const teacher = faker.helpers.arrayElement(teachers);
+      await prisma.trainingPlan.create({
+        data: {
+          name: faker.helpers.arrayElement([
+            "Treino para hipertrofia",
+            "Treino para emagrecimento",
+            "Treino para força",
+          ]),
+          studentId: student.id,
+          teacherId: teacher.id,
+          teacherEmail: teacher.email,
+          teacherName: teacher.name,
+          trainings: {
+            create: [
+              {
+                name: "Treino A",
+                trainingExercises: { create: exercisesTrainigsA },
+              },
+              {
+                name: "Treino B",
+                trainingExercises: { create: exercisesTrainigsB },
+              },
+              {
+                name: "Treino C",
+                trainingExercises: { create: exercisesTrainigsC },
+              },
+              {
+                name: "Treino D",
+                trainingExercises: { create: exercisesTrainigsD },
+              },
+            ],
           },
-        });
-      }
+        },
+      });
     }
   }
 }
